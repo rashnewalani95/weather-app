619f687cb4eb3744f0364ff92ac3fbfc1fb4a594
rashnewalani95 <rashminewalani@gmail.com>
2023-10-18 18:49:51 +0200

    Added Java Weekly forecast

--- src/app.js
+++ src/app.js
@@ -15,8 +15,57 @@ function formatDate (timestamp) {
   let day = days[date.getDay()];
 
   return `${day} ${hours} : ${minutes}`;
-
 }
+
+function displayForecast(){
+   let forecastElement = document.querySelector("#forecast");
+
+let forecastHTML = `<div class = "row">`;
+let days = ["Thu","Fri","Sat","Sun"];
+
+days.forEach(function (day){
+   forecastHTML = forecastHTML + ` <div class = "col-2">
+                <div class = "weather-forecast-date">
+                 ${day}
+                 </div>
+                 <img src="http://openweathermap.org/img/wn/50d@2x.png"
+                 alt=""
+                 width="50"
+                 />
+                 <div class = "weather-forecast-temperature">
+                 <span class = "weather-forecast-temperature-max">
+                    18° </span>
+                 <span class = "weather-forecast-temperature-min">
+                 12° </span>
+                 </div>
+            </div>
+        `;
+});
+        forecastHTML = forecastHTML+`</div>`;
+        forecastElement.innerHTML = forecastHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -103,3 +152,4 @@ let celsiusTemperature = null;
 
 
   search("Durban");
+  displayForecast();
